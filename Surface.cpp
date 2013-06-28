667b42671ade39314519319df943a09b39fd3924
gleb <muteelectron@gmail.com>
2013-06-28 20:13:30 +0300

    Some progress

--- Surface.cpp
+++ Surface.cpp
@@ -2,35 +2,36 @@
 #include <fstream>
 
 
-SDL_Surface *Surface::OnLoad( char *File )
+SDL_Surface *Surface::OnLoad(char *File)
 {
     SDL_Surface* Surf_Temp = NULL;
     SDL_Surface* Surf_Return = NULL;
-    if( ( Surf_Temp = IMG_Load( File ) ) == NULL )
+    if((Surf_Temp = IMG_Load(File)) == NULL)
     {
         return NULL;
     }
-    Surf_Return = SDL_DisplayFormatAlpha( Surf_Temp );
-    SDL_FreeSurface( Surf_Temp );
+    Surf_Return = SDL_DisplayFormatAlpha(Surf_Temp);
+    SDL_FreeSurface(Surf_Temp);
     return Surf_Return;
 };
 
-bool Surface::OnDraw( SDL_Surface* Surf_Dest, SDL_Surface* Surf_Src, int X, int Y )
+bool Surface::OnDraw(SDL_Surface* Surf_Dest, SDL_Surface* Surf_Src, int X, int Y)
 {
-    if( Surf_Dest == NULL || Surf_Src == NULL )
+    if(Surf_Dest == NULL || Surf_Src == NULL)
     {
         return false;
     }
     SDL_Rect DestR;
     DestR.x = X;
     DestR.y = Y;
-    SDL_BlitSurface( Surf_Src, NULL, Surf_Dest, &DestR );
+    SDL_BlitSurface(Surf_Src, NULL, Surf_Dest, &DestR);
     return true;
 };
 
-bool Surface::OnDraw( SDL_Surface* Surf_Dest, SDL_Surface* Surf_Src, int X, int Y, int X2, int Y2, int W, int H )
+bool Surface::OnDraw(SDL_Surface* Surf_Dest, SDL_Surface* Surf_Src, int X,
+                     int Y, int X2, int Y2, int W, int H)
 {
-    if( Surf_Dest == NULL || Surf_Src == NULL )
+    if(Surf_Dest == NULL || Surf_Src == NULL)
     {
         return false;
     }
@@ -42,6 +43,6 @@ bool Surface::OnDraw( SDL_Surface* Surf_Dest, SDL_Surface* Surf_Src, int X, int 
     SrcR.y = Y2;
     SrcR.w = W;
     SrcR.h = H;
-    SDL_BlitSurface( Surf_Src, &SrcR, Surf_Dest, &DestR );
+    SDL_BlitSurface(Surf_Src, &SrcR, Surf_Dest, &DestR);
     return true;
 }
--- Surface.h
+++ Surface.h
@@ -17,9 +17,10 @@ class Surface
 {
    public:
 
-      static SDL_Surface* OnLoad( char* File );
-      static bool OnDraw( SDL_Surface* Surf_Dest, SDL_Surface* Surf_Src, int X, int Y );
-      static bool OnDraw( SDL_Surface* Surf_Dest, SDL_Surface* Surf_Src, int X, int Y, int X2, int Y2, int W, int H );
+      static SDL_Surface* OnLoad(char* File);
+      static bool OnDraw(SDL_Surface* Surf_Dest, SDL_Surface* Surf_Src, int X, int Y);
+      static bool OnDraw(SDL_Surface* Surf_Dest, SDL_Surface* Surf_Src, int X, int Y, 
+                         int X2, int Y2, int W, int H);
 };
 
 #endif
